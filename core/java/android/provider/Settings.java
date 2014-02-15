b70533c230a66f65e735c98c29e03c9e2d0f1d48
TheWolf91 <the_wolf91@libero.it>
2014-02-15 19:32:05 +0100

    Revert "Enable/Disable NavBar [1/2]"
    This reverts commit 3a53a7267b96b7b4a1c32b43621f88b0c9e2adc1.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3452,12 +3452,6 @@ public final class Settings {
         public static final String FORCE_SHOW_OVERFLOW_MENU = "force_show_overflow_menu";
 
         /**
-         * Setting to force show navigation bar.
-         * @hide
-         */
-        public static final String SHOW_NAVIGATION = "show_navigation";
-
-        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -317,11 +317,7 @@ public abstract class BaseStatusBar extends SystemUI implements
                 Settings.Global.getUriFor(Settings.Global.DEVICE_PROVISIONED), true,
                 mProvisioningObserver);
 
-        mContext.getContentResolver().registerContentObserver(
-                Settings.System.getUriFor(Settings.System.SHOW_NAVIGATION), true,
-                mShowNavObserver, UserHandle.USER_ALL);
-
-        mContext.getContentResolver().registerContentObserver(
+	mContext.getContentResolver().registerContentObserver(
                 Settings.System.getUriFor(Settings.System.LARGE_RECENT_THUMBS), true,
                 mShowNavObserver, UserHandle.USER_ALL);
 
