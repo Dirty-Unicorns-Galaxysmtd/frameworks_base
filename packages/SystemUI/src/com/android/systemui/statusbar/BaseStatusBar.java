db21f39161fdf29a295269ab507fd967befe412d
Alex Cruz <mazdarider23@gmail.com>
2014-02-14 18:36:26 -0600

    Enable/Disable NavBar [1/2]
    Credits/Thanks to AOKP
    
    Change-Id: I830c8009e8a84953b092fcd67e75431b4ad10ce6

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -317,7 +317,11 @@ public abstract class BaseStatusBar extends SystemUI implements
                 Settings.Global.getUriFor(Settings.Global.DEVICE_PROVISIONED), true,
                 mProvisioningObserver);
 
-	mContext.getContentResolver().registerContentObserver(
+        mContext.getContentResolver().registerContentObserver(
+                Settings.System.getUriFor(Settings.System.SHOW_NAVIGATION), true,
+                mShowNavObserver, UserHandle.USER_ALL);
+
+        mContext.getContentResolver().registerContentObserver(
                 Settings.System.getUriFor(Settings.System.LARGE_RECENT_THUMBS), true,
                 mShowNavObserver, UserHandle.USER_ALL);
 
