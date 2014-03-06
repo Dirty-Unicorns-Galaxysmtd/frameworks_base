7d305a7fdc3c0c888c870269d7f89bd58806a599
TheWolf91 <the_wolf91@libero.it>
2014-03-06 01:30:14 +0100

    Trying to fix Blur for galaxysmtd

--- packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
@@ -108,6 +108,7 @@ public class KeyguardViewManager {
     private AudioManager mAudioManager;
 
     private Drawable mCustomBackground = null;
+    private Bitmap mBlurredImage = null;
     private int mBlurRadius = 14;
     private boolean mSeeThrough = false;
 
